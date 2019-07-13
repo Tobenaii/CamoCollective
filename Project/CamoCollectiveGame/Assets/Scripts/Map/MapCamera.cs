67d6c4151b06afff0e092602c29ea7d0e20f8ad2
Tobenai <akuapengin42@gmail.com>
2019-07-13 15:30:23 +1000

    GameMode camera pan implemented

---
+++ Project/CamoCollectiveGame/Assets/Scripts/Map/MapCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float m_panSpeed;
+    private GameObject m_currentTarget;
+    private bool m_atTarget;
+    private Vector3 m_velocity;
+    private float m_panTime;
+
+    private void Start()
+    {
+        m_atTarget = true;
+    }
+
+    private void Update()
+    {
+        if (m_atTarget)
+            return;
+
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(m_currentTarget.transform.position.x, transform.position.y, m_currentTarget.transform.position.z), ref m_velocity, 1 / m_panSpeed * m_panTime);
+        if (Vector3.Distance(transform.position, m_currentTarget.transform.position) < 0.001f)
+            m_atTarget = true;
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        m_currentTarget = target;
+        m_atTarget = false;
+        m_panTime = 1 / Vector3.Magnitude(transform.position - target.transform.position);
+    }
+}
---
+++ Project/CamoCollectiveGame/Assets/Scripts/ModularSystems/EventListener/ButtonEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonEvent : MonoBehaviour
+{
+    [SerializeField]
+    private GameObjectEvent m_event;
+    [SerializeField]
+    private GameObject m_targetGameObject;
+    private bool m_invoked;
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == gameObject && !m_invoked)
+        {
+            m_invoked = true;
+            m_event.Invoke(m_targetGameObject);
+        }
+        else if (EventSystem.current.currentSelectedGameObject != gameObject)
+            m_invoked = false;
+    }
+}
