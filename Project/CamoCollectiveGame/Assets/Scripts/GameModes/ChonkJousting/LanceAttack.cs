02a98c3072fde15e8580dd3cc416d9b42cfa18ea
Tobenai <akuapengin42@gmail.com>
2019-07-17 13:27:15 +1000

    Jousting successful hit adds to score

--- Project/CamoCollectiveGame/Assets/Scripts/GameModes/ChonkJousting/LanceAttack.cs
+++ Project/CamoCollectiveGame/Assets/Scripts/GameModes/ChonkJousting/LanceAttack.cs
@@ -9,21 +9,24 @@ public class LanceAttack : MonoBehaviour
     [SerializeField]
     private FloatValue m_livesValue;
     [SerializeField]
+    private FloatValue m_scoreValue;
+    [SerializeField]
     private float m_shieldAngle;
 
     private void Awake()
     {
         m_livesValue.value = 4;
+        m_scoreValue.value = 0;
     }
     private void OnTriggerEnter(Collider other)
     {
         LanceAttack chonk = other.transform.parent.GetComponent<LanceAttack>();
         if (chonk == null)
             return;
-        chonk.Attack(gameObject);
+        chonk.Attack(gameObject, m_scoreValue);
     }
 
-    public void Attack(GameObject other)
+    public void Attack(GameObject other, FloatValue otherScore)
     {
         float dot = Vector3.Dot(transform.forward, other.transform.forward);
         float shield = Mathf.Lerp(-1, 1, Mathf.InverseLerp(0, 180, m_shieldAngle));
@@ -31,5 +34,6 @@ public class LanceAttack : MonoBehaviour
             return;
         GetComponent<Rigidbody>().AddForce(other.transform.forward * m_knockbackForce, ForceMode.Impulse);
         m_livesValue.value--;
+        otherScore.value++;
     }
 }
