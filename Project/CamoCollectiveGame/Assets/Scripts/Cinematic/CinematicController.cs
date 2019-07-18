2c507501bca01cdd8eae81dc43f3841ac4c9835b
Tobenai <akuapengin42@gmail.com>
2019-07-18 16:16:10 +1000

    Initial cinematic system implemented

---
+++ Project/CamoCollectiveGame/Assets/Scripts/Cinematic/CinematicController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(StringEventListener))]
+public class CinematicController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator m_animator;
+    [SerializeField]
+    private string startupCinematic;
+
+    private void Start()
+    {
+        StartCinematic(startupCinematic);
+    }
+
+    public void StartCinematic(string cinematic)
+    {
+        if (cinematic == "StopCinematics")
+        {
+            m_animator.enabled = false;
+            return;
+        }
+        m_animator.enabled = true;
+        m_animator.Play(cinematic, 0);
+    }
+}
