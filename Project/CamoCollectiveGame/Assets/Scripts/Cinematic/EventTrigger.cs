e1d3bb1763d64bfcc3d7ce7341135d474f06102c
Tobenai <akuapengin42@gmail.com>
2019-07-18 16:31:37 +1000

    Animation triggers for start/update/finished added

---
+++ Project/CamoCollectiveGame/Assets/Scripts/Cinematic/EventTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger : StateMachineBehaviour
+{
+    [SerializeField]
+    private GameEvent m_onStart;
+    [SerializeField]
+    private GameEvent m_onUpdate;
+    [SerializeField]
+    private float m_frame;
+    [SerializeField]
+    private GameEvent m_onComplete;
+    private float m_currentFrame;
+
+
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //Invoke onstart at start of animation
+        if (m_onStart)
+            m_onStart.Invoke();
+        m_currentFrame = 0;
+    }
+
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //Invoke onUpdate on specified time
+        if (m_onUpdate)
+        {
+            if (m_currentFrame >= m_frame)
+                m_onUpdate.Invoke();
+        }
+        m_currentFrame += Time.deltaTime;
+    }
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //invoke onComplete when animation state exits
+        if (m_onComplete)
+            m_onComplete.Invoke();
+    }
+}
---
+++ Project/CamoCollectiveGame/Assets/Scripts/SceneLoadOnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadOnTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private string m_sceneName;
+    public void LoadScene()
+    {
+        SceneManager.LoadSceneAsync(m_sceneName, LoadSceneMode.Additive);
+    }
+}
