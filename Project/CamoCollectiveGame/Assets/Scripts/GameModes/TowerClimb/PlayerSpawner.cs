9eb3e76476f766f00896f76d972cd621e3810309
Tobenai <akuapengin42@gmail.com>
2019-07-21 13:57:22 +1000

    Tower Climb now scales with ammount of players

---
+++ Project/CamoCollectiveGame/Assets/Scripts/GameModes/TowerClimb/PlayerSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> m_spawns;
+    [SerializeField]
+    private List<PlayerData> m_playerData;
+    [SerializeField]
+    private GameObject m_playerPrefab;
+
+    private void Awake()
+    {
+        int index = 0;
+        foreach (PlayerData player in m_playerData)
+        {
+            if (!player.IsPlaying())
+                continue;
+            InputMapper input = Instantiate(m_playerPrefab, m_spawns[index].position, m_spawns[index].rotation, transform).GetComponent<InputMapper>();
+            input.SetControllerNum(index);
+            index++;
+        }
+    }
+}
