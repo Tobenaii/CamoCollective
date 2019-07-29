23df1ce99fa61179da5c96d3c6fc057b8a840356
Tobenai <akuapengin42@gmail.com>
2019-07-29 16:24:28 +1000

    Simple 'Main Menu' implemented

---
+++ Project/CamoCollectiveGame/Assets/Scripts/ModularSystems/EventListener/StringEventTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringEventTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private StringEvent m_stringEvent;
+
+    public void Invoke(string s)
+    {
+        m_stringEvent.Invoke(s);
+    }
+}
