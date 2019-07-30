7f084ce3dbd0a698e7dca3024b0cca3437bd9044
Tobenai <akuapengin42@gmail.com>
2019-07-30 18:32:08 +1000

    Main Menu scene loads on start

--- Project/CamoCollectiveGame/Assets/Scripts/ModularSystems/SceneLoader.cs
+++ Project/CamoCollectiveGame/Assets/Scripts/ModularSystems/SceneLoader.cs
@@ -7,6 +7,20 @@ public class SceneLoader: MonoBehaviour
 {
     [SerializeField]
     private List<SceneValue> m_scenes;
+    [SerializeField]
+    private bool m_loadOnAwake;
+
+    private void Awake()
+    {
+        if (!m_loadOnAwake)
+            return;
+        foreach (SceneValue scene in m_scenes)
+        {
+            if (!SceneManager.GetSceneByName(scene).IsValid())
+                SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+        }
+    }
+
     public void LoadScene()
     {
         foreach (SceneValue scene in m_scenes)
