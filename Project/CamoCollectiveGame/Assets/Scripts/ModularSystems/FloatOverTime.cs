1471bce84b75d904ef8d1704136ab7db70cd2619
Tobenai <akuapengin42@gmail.com>
2019-08-01 19:33:32 +1000

    Tower Climb speed increases over time

---
+++ Project/CamoCollectiveGame/Assets/Scripts/ModularSystems/FloatOverTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatOverTime : MonoBehaviour
+{
+    [SerializeField]
+    private FloatValue m_float;
+    [SerializeField]
+    private float m_increaseRate;
+    [SerializeField]
+    private bool m_cap;
+    [SerializeField]
+    private float m_maxCap;
+
+    private void Update()
+    {
+        m_float.value += m_increaseRate * Time.deltaTime;
+        if (m_cap && m_float.value > m_maxCap)
+            m_float.value = m_maxCap;
+    }
+}
---
+++ Project/CamoCollectiveGame/Assets/Scripts/ModularSystems/Variable/InitFloatValue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitFloatValue : MonoBehaviour
+{
+    [SerializeField]
+    private float m_initValue;
+    [SerializeField]
+    private FloatValue m_floatValue;
+
+    private void Awake()
+    {
+        m_floatValue.value = m_initValue;
+    }
+}
