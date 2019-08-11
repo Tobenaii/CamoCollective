586f438f97766945b45d03f4b8bb609580ec3d37
Tobenai <akuapengin42@gmail.com>
2019-08-11 19:45:37 +1000

    Implemented side and top barriers for new Tower Climb

--- Project/CamoCollectiveGame/Assets/Scripts/ModularSystems/GameObjectMover.cs
+++ Project/CamoCollectiveGame/Assets/Scripts/ModularSystems/GameObjectMover.cs
@@ -7,9 +7,7 @@ public class GameObjectMover : MonoBehaviour
     [SerializeField]
     private Vector3 m_direction;
     [SerializeField]
-    private float m_moveSpeed;
-    [SerializeField]
-    private FloatValue m_scale;
+    private FloatReference m_moveSpeed;
     [SerializeField]
     private bool m_moveOnAwake;
     private bool m_move;
@@ -34,6 +32,6 @@ public class GameObjectMover : MonoBehaviour
     {
         if (!m_move)
             return;
-        transform.position += m_direction.normalized * m_moveSpeed * m_scale.Value * Time.deltaTime;
+        transform.position += m_direction.normalized * m_moveSpeed.Value * Time.deltaTime;
     }
 }
