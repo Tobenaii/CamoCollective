c52b7b385130e06fde0d2e9ac1f9b686d5b95a30
Tobenaii <akuapengin42@gmail.com>
2019-09-13 11:52:55 +1000

    Game Mode ready up check implemented

---
+++ Project/CamoCollectiveGame/Assets/Scripts/GameModes/GameModeReadyUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameModeReadyUp : MonoBehaviour
+{
+    [SerializeField]
+    private int m_playerNum;
+    [SerializeField]
+    private PlayerData m_player;
+    [SerializeField]
+    private GameEvent m_playerReadyEvent;
+    [SerializeField]
+    private Text m_playerStatusText;
+
+    private void Awake()
+    {
+        m_playerStatusText.color = m_player.IndicatorColour;
+    }
+
+    public void ReadyUp()
+    {
+        m_playerReadyEvent.Invoke();
+        m_playerStatusText.text = "Player " + m_playerNum + " Ready!";
+    }
+}
---
+++ Project/CamoCollectiveGame/Assets/Scripts/GameModes/GameModeReadyUpCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModeReadyUpCheck : MonoBehaviour
+{
+    [SerializeField]
+    private List<PlayerData> m_players;
+    [SerializeField]
+    private GameEvent m_everyoneReadyEvent;
+
+    private int m_playersPlaying;
+    private int m_playersReady;
+
+    private void Start()
+    {
+        foreach (PlayerData player in m_players)
+            m_playersPlaying += Convert.ToInt32(player.IsPlaying);
+    }
+
+    public void ReadyUpCheck()
+    {
+        m_playersReady++;
+        if (m_playersReady == m_playersPlaying)
+            m_everyoneReadyEvent.Invoke();
+    }
+}
