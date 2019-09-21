a0caac6e4654261681530b99ba357060dfc1d253
Tobenaii <akuapengin42@gmail.com>
2019-09-21 15:13:28 +1000

    Character Select now only lets you continue if all players have chosen a character

---
+++ Project/CamoCollectiveGame/Assets/Scripts/CharacterSelect/CharacterSelectBeginCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SceneLoader))]
+public class CharacterSelectBeginCheck : MonoBehaviour
+{
+    [SerializeField]
+    private List<PlayerData> m_players;
+
+    public void Begin()
+    {
+        bool begin = true;
+        foreach (PlayerData player in m_players)
+        {
+            if (player.IsPlaying && player.Character == null)
+            {
+                begin = false;
+                break;
+            }
+        }
+        if (begin)
+            GetComponent<SceneLoader>().Load();
+    }
+}
