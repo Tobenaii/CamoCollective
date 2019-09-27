2636cf4dd57fae643968181694a31651349ff4f2
Tobenaii <akuapengin42@gmail.com>
2019-09-28 05:55:16 +1000

    Revert "Fixed infiniscroll reinit bug"
    This reverts commit 218dfcf2fcf0c4058ac6d6291a176f1f3da5e25b.

--- Project/CamoCollectiveGame/Assets/Scripts/ModeSelection/SelectionScroller.cs
+++ Project/CamoCollectiveGame/Assets/Scripts/ModeSelection/SelectionScroller.cs
@@ -29,7 +29,6 @@ public class SelectionScroller : MonoBehaviour
 
     private void Start()
     {
-        m_currentSelectionValue.Value = (int)Mathf.Repeat(m_currentSelectionValue.Value, m_selections.Count);
         m_rect = GetComponent<RectTransform>();
         m_rect.anchoredPosition = new Vector3((m_currentSelectionValue.Value - 1) * -m_spacing, m_rect.anchoredPosition.y, 0);
         InfiniteScroll();
@@ -49,14 +48,6 @@ public class SelectionScroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (EventSystem.current.currentSelectedGameObject == null)
-        {
-            int curIndex = (int)Mathf.Repeat(m_currentSelectionValue.Value, m_selections.Count);
-            EventSystem.current.SetSelectedGameObject(m_selections[curIndex]);
-            UpdateTransforms();
-        }
-
         Vector2 newPos = new Vector2((m_currentSelectionValue.Value - 1) * -m_spacing, m_rect.anchoredPosition.y);
         if (m_rect.anchoredPosition != newPos)
         {
