ac8a13581d8c5a5958b4f16d58a356f4a70576ad
Tobenaii <akuapengin42@gmail.com>
2019-10-04 14:04:03 +1000

    TC - Implemented distance tracker

---
+++ Project/CamoCollectiveGame/Assets/Scripts/GameModes/TowerClimb/TowerClimbDistanceTracker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerClimbDistanceTracker : MonoBehaviour
+{
+    [SerializeField]
+    private FloatValue m_climbSpeed;
+    [SerializeField]
+    private float m_scale;
+    [SerializeField]
+    private Text m_text;
+
+    private float m_distance;
+    private bool m_stopped;
+
+    private void Update()
+    {
+        if (m_stopped)
+            return;
+
+        m_distance += m_climbSpeed.Value * Time.deltaTime * m_scale;
+        m_text.text = (int)m_distance + "m";
+    }
+
+    public void Stop()
+    {
+        m_stopped = true;
+    }
+}
