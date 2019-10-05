6d9d963971d70c156dd553f4d93f3f4aef753ab7
BlakeKolodziej <blakekolodziej@gmail.com>
2019-10-05 14:48:53 +1000

    player banners + other stuff

--- Project/CamoCollectiveGame/Assets/Scripts/Data/PlayerData.cs
+++ Project/CamoCollectiveGame/Assets/Scripts/Data/PlayerData.cs
@@ -15,11 +15,14 @@ public class PlayerData : ScriptableObject
     private bool m_isPlaying;
     [SerializeField]
     private Color m_indicatorColour;
+    [SerializeField]
+    private Color32 m_bannerColour;
 
     public CharacterData Character { get { return m_currentCharacter; } set { if (m_currentCharacter != null) m_currentCharacter.inUse = false; m_currentCharacter = value; if (m_currentCharacter != null) m_currentCharacter.inUse = true; } }
     public CharacterData ForcedCharacter { get { return m_forcedCharacter; } private set { } }
     public bool IsPlaying { get { return m_isPlaying; } set { m_isPlaying = value; } }
     public Color IndicatorColour { get { return m_indicatorColour; } private set { } }
+    public Color BannerColour { get { return m_bannerColour; } private set { } }
     public CharacterData GhostCharacter { get { return m_ghostCharacter; } private set { } }
 
     public void SetGhostCharacter(CharacterData character)
