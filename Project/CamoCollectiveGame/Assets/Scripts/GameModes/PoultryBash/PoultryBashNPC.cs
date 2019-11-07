acbf8d0d81f9d88919c1560f2da8159cc4b62f99
Tobenaii <akuapengin42@gmail.com>
2019-11-07 11:26:21 +1100

    PB - NPC's in arena

---
+++ Project/CamoCollectiveGame/Assets/Scripts/GameModes/PoultryBash/PoultryBashNPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoultryBashNPC : MonoBehaviour
+{
+    private Animator m_animator;
+
+    private void Awake()
+    {
+        m_animator = GetComponentInChildren<Animator>();
+        m_animator.speed = Random.Range(1.0f, 1.5f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
