85e595280debbc2058706bf5e9cae21ada9c9aa0
Tobenaii <akuapengin42@gmail.com>
2019-11-07 12:09:23 +1100

    PB - NPC ass scratching anim plays at random invtervals

--- Project/CamoCollectiveGame/Assets/Scripts/GameModes/PoultryBash/PoultryBashNPC.cs
+++ Project/CamoCollectiveGame/Assets/Scripts/GameModes/PoultryBash/PoultryBashNPC.cs
@@ -7,10 +7,18 @@ public class PoultryBashNPC : MonoBehaviour
     private Animator m_animator;
     private Material m_material;
 
+    private float m_assScratchTimer; 
+
     private void Awake()
     {
         m_animator = GetComponentInChildren<Animator>();
         m_animator.speed = Random.Range(1.0f, 1.5f);
+        GetRandomAssScratch();
+    }
+
+    private void GetRandomAssScratch()
+    {
+        m_assScratchTimer = Random.Range(5, 30);
     }
 
     public void Cheer()
@@ -27,6 +35,11 @@ public class PoultryBashNPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        m_assScratchTimer -= Time.deltaTime;
+        if (m_assScratchTimer <= 0)
+        {
+            m_animator.SetTrigger("AssScratch");
+            GetRandomAssScratch();
+        }
     }
 }
