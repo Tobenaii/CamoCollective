3e422551b491b9658d91421f541d535cab633497
Tobenaii <akuapengin42@gmail.com>
2019-11-07 11:40:07 +1100

    PB - NPC's cheer on round end

--- Project/CamoCollectiveGame/Assets/Scripts/GameModes/PoultryBash/PoultryBashNPC.cs
+++ Project/CamoCollectiveGame/Assets/Scripts/GameModes/PoultryBash/PoultryBashNPC.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PoultryBashNPC : MonoBehaviour
 {
     private Animator m_animator;
+    private Material m_material;
 
     private void Awake()
     {
         m_animator = GetComponentInChildren<Animator>();
         m_animator.speed = Random.Range(1.0f, 1.5f);
+    }
+
+    public void Cheer()
+    {
+        m_animator.SetTrigger("Cheer");
     }
 
     // Start is called before the first frame update
