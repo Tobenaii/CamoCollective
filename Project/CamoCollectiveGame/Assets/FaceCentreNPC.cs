3a15b572196c2c16ec9e0a0361fea6dc2159f458
BlakeKolodziej <blakekolodziej@gmail.com>
2019-11-12 22:46:46 +1100

    PB - Moved some NPCs (need to finish), added NPC face stage script

---
+++ Project/CamoCollectiveGame/Assets/FaceCentreNPC.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCentreNPC : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_lookTarget;
+
+
+    private void Awake()
+    {
+        transform.LookAt(new Vector3(m_lookTarget.transform.position.x, transform.position.y, m_lookTarget.transform.position.z));
+    }
+}
