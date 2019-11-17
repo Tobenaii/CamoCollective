4bed4cbb0891781dfbc86e9f73a88995f99bca9c
Tobenaii <akuapengin42@gmail.com>
2019-11-17 18:28:55 +1100

    Canvas fade in

---
+++ Project/CamoCollectiveGame/Assets/Scripts/ModularSystems/CanvasFade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class CanvasFade : MonoBehaviour
+{
+    [SerializeField]
+    private float m_fadeInSpeed;
+
+    private CanvasGroup m_canvasGroup;
+    private float m_opacity;
+
+    private void OnEnable()
+    {
+        m_opacity = 0;
+    }
+
+    private void Start()
+    {
+        m_canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void Update()
+    {
+        m_canvasGroup.alpha = m_opacity;
+        m_opacity += m_fadeInSpeed * Time.deltaTime;
+        m_opacity = Mathf.Clamp(m_opacity, 0, 1);
+    }
+}
