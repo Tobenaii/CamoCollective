f0f74666a00a27cda92cba9a4294262aa09393d7
Tobenaii <akuapengin42@gmail.com>
2019-11-18 14:39:07 +1100

    Story Mode - Better character entry

--- Project/CamoCollectiveGame/Assets/Scripts/StoryMode/StoryModeCharacterController.cs
+++ Project/CamoCollectiveGame/Assets/Scripts/StoryMode/StoryModeCharacterController.cs
@@ -8,27 +8,44 @@ public class StoryModeCharacterController : MonoBehaviour
     private GameObject m_gainedPointText;
     [SerializeField]
     private GameObjectListSet m_targetListSet;
+    private Animator m_animator;
     private Vector3 m_firstTarget;
     private Vector3 m_target;
     private bool m_doTheShuffle;
+    private Vector3 m_velocity;
+    private bool m_stopped;
 
     private void Start()
     {
         m_target = m_targetListSet[0].transform.position;
         m_targetListSet.RemoveAt(0);
-        m_firstTarget = new Vector3(transform.position.x, transform.position.y, m_target.z);
+        m_firstTarget = new Vector3(transform.position.x, transform.position.y, m_target.z - (m_target.z - transform.position.z) / 2);
+
+        m_animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
+        if (m_stopped)
+        {
+            transform.forward = Vector3.MoveTowards(transform.forward, Vector3.back, 10 * Time.deltaTime);
+            return;
+        }
+        transform.forward = m_velocity;
         if (!m_doTheShuffle)
         {
-            transform.position = Vector3.MoveTowards(transform.position, m_firstTarget, 2 * Time.deltaTime);
-            if (Vector3.Distance(transform.position, m_firstTarget) < 0.01f)
+            transform.position = Vector3.SmoothDamp(transform.position, m_firstTarget - Vector3.forward * 10, ref m_velocity, 4.0f);
+            if (Vector3.Distance(transform.position, m_firstTarget) < 0.1f)
                 m_doTheShuffle = true;
         }
+        else if (Vector3.Distance(transform.position, m_target) > 0.5f)
+            transform.position = Vector3.SmoothDamp(transform.position, m_target, ref m_velocity, 1.0f);
         else
-            transform.position = Vector3.MoveTowards(transform.position, m_target, 2 * Time.deltaTime);
+        {
+            m_stopped = true;
+            m_animator.SetTrigger("Idle");
+            m_animator.speed = 1;
+        }
     }
 
     public void GainPoint()
