2b15d2887e6269a559054b5810a19985f9f48f8e
Tobenaii <akuapengin42@gmail.com>
2019-11-21 10:02:33 +1100

    Fixed herbert robe

---
+++ Project/CamoCollectiveGame/Assets/Scripts/QualityMode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QualityMode : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEngine.Audio.AudioMixer m_audioMixer;
+
+    private void Awake()
+    {
+        if (!PlayerPrefs.HasKey("Quality"))
+            PlayerPrefs.SetFloat("Quality", 6);
+        if (!PlayerPrefs.HasKey("Sound"))
+            PlayerPrefs.SetFloat("Sound", 10);
+        if (!PlayerPrefs.HasKey("Music"))
+            PlayerPrefs.SetFloat("Music", 10);
+    }
+}
